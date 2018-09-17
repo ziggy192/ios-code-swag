238f01d4f9999916bb67b5f4ea805fbc666682b9
NghiaLQ <luuquangnghia97@gmail.com>
2018-09-17 20:06:30 +0700

    Initial Commit

---
+++ code-swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  code-swag
+//
+//  Created by Nghia Luu on 9/17/18.
+//  Copyright © 2018 Nghia Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
